b7b9c7dc469651f12efc237d94861e219690045f
Abhi Savaliya <abhisavaliya01@gmail.com>
2019-10-06 21:06:23 -0400

    changed readme and uploaded to pypi

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# hsv_calibration
+# HSV Calibration
+
+Wth `HSV Calibration`, get the accurate `HSV Upper and Lower Range` from the `image`,`webcam`, and ,`mobile camera` (using IPCam) which can be used for `color detection`, `object tracking` and much more.
+
+# Detect Colors Using:
+- `Local Images` (Provide *url*)
+- `Webcam`
+- `Mobile Camera` (Provide *IP address* using IPCam)
+
+# Examples
+
+1). `Images`
+```sh
+from hsv-calibration import ProcessImage
+ProcessImage.process_image("C:\Downloads\image.jpg")
+```
+2). `Webcam`
+```sh
+from hsv-calibration import ProcessImage
+ProcessImage.process_webcam()
+```
+3). `Mobile Camera (Use IPCam to get the IP Address)`
+```sh
+from hsv-calibration import ProcessImage
+ProcessImage.process_mobile_camera("http://192.168.2.15:8080/")
+```
+
+# New Features!
+- `Brightness`
+- `Darkness`
+- `Blur`
+    - `Box Blur`
+    - `Gaussian Blur`
+    - `Median Blur`
+    - `Bilateral Blur`
+- `Hue (Low and High)`
+- `Saturation (Low and High)`
+- `Value (Low and High)`
+- `Erosion`
+- `Dilation`
+- `Edge Detection using Canny`
+
+# Why did I build this?
+
+> To recognize or detect objects or colors in the image, OpenCV requires `HSV lower and upper range` to detect the color.
+> This is time consuming as to get the accurate range, we go with hit nd trial method.
+> With this library, you can easily get the accurate range and best results using other features as well.
+
+
+# Requirements/Dependencies
+| Requirements | Install |
+| ------ | ------ |
+| numpy | ` pip install numpy ` |
+| opencv | ` pip install opencv-python ` |
+| requests | ` pip install requests` |
+| hsv-calibration | ` pip install hsv-calibration` |
+        
+# Installation
+
+```sh 
+pip install hsv-calibration 
+```
+
+# Feedback and Suggestions
+For feedback and suggestions, please email me on `abhisavaliya01@gmail.com`
+
+# About Author
+
+`Abhi Savaliya` | `abhisavaliya01@gmail.com` | [GitHub](`https://github.com/abhisavaliya`) | [LinkedIn](`https://www.linkedin.com/in/abhisavaliya/`)
---
+++ setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  6 16:34:03 2019
+
+@author: abhis
+"""
+
+from distutils.core import setup
+setup(
+  name = 'hsv_calibration',         
+  packages = ['hsv_calibration'],
+  version = '0.1',
+  license='MIT',  
+  description = 'Accurate values for HSV lower and upper range',  
+  author = 'Abhi Savaliya',                 
+  author_email = 'abhisavaliya01@gmail.com',    
+  url = 'https://github.com/abhisavaliya/hsv_calibration/',  
+  download_url = 'https://github.com/abhisavaliya/hsv_calibration/archive/v1.0-beta.0.tar.gz',
+  keywords = ['OpenCV','HSV','Calibration','tool','edge','detection',
+              'color','range','upper','lower','dilation','hue','saturation',
+              'blur','box','gaussian','median','bilateral','bright',
+              'dark','brightness','darkness','mask','erosion','opening',
+              'closing','canny','threshold','easy','machine','learning',
+              'image','recognition','classification','processing','computer',
+              'vision'],
+  install_requires=[           
+          'requests',
+          'numpy',
+          'opencv-python'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Testing',
+    'Topic :: Software Development :: Build Tools',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.1',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
